64fd1f8ab359f234ae792b6d8b64e88790b6c58f
Winter4 <mywinter4@gmail.com>
2022-05-05 00:30:58 +0300

    dotenv path

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 
-require('dotenv').config({ path: 'C:/Users/Vladimir/Desktop/calories-app/.env' });
+require('dotenv').config({ path: '../.env' });
 
 // _________________________________________
 
